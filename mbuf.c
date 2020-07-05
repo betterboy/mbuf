df10f41daa5d6b26007b9146af036354c416e925
zhuyankong <betterboy3@163.com>
2020-07-05 04:15:58 -0400

    #修复mbuf_drain导致blk_deq失效问题

--- mbuf.c
+++ mbuf.c
@@ -3,11 +3,11 @@
 
 #include <errno.h>
 
-#define MIN_BLK_SIZE    (4)
-#define BLK_FACTOR  (4)
+#define MIN_BLK_SIZE (4)
+#define BLK_FACTOR (4)
 
 //默认的blk大小
-#define BLK_SIZE    (2048)
+#define BLK_SIZE (2048)
 
 static int can_log(mbuf_t *mbuf)
 {
@@ -19,12 +19,15 @@ inline static void mbuf_blk_init(mbuf_blk_t *blk)
     blk->head = blk->tail = blk->buf;
 }
 
-static void mbuf_reset(mbuf_t *mbuf, uint32_t new_size)
+void mbuf_reset(mbuf_t *mbuf, uint32_t new_size)
 {
-    if (mbuf->blk_count > 1 || mbuf->alloc_size <= new_size) {
+    if (mbuf->blk_count > 1 || mbuf->alloc_size <= new_size)
+    {
         mbuf_free(mbuf);
         mbuf_init(mbuf, (new_size > mbuf->alloc_size) ? new_size : mbuf->alloc_size);
-    } else {
+    }
+    else
+    {
         assert(mbuf->blk_count == 1);
         mbuf_blk_init(mbuf->blk_enq);
     }
@@ -35,7 +38,8 @@ mbuf_blk_t *mbuf_add_blk(mbuf_t *mbuf, uint32_t size)
     mbuf_blk_t *blk = (mbuf_blk_t *)malloc(sizeof(mbuf_blk_t));
     size = mbuf->hint_size > size ? mbuf->hint_size : size;
     size = BLK_FACTOR * ((size + BLK_FACTOR - 1) / BLK_FACTOR);
-    if (size < MIN_BLK_SIZE) {
+    if (size < MIN_BLK_SIZE)
+    {
         size = MIN_BLK_SIZE;
     }
 
@@ -46,16 +50,22 @@ mbuf_blk_t *mbuf_add_blk(mbuf_t *mbuf, uint32_t size)
     blk->end = blk->buf + blk->blk_size;
     blk->head = blk->tail = blk->buf;
     blk->next = NULL;
-    
-    if (mbuf->blk_enq != NULL) {
+
+    if (mbuf->blk_enq != NULL)
+    {
         mbuf->blk_enq->next = blk;
     }
 
     mbuf->blk_enq = blk;
     mbuf->alloc_size += size;
 
+    if (mbuf->blk_deq == NULL)
+    {
+        mbuf->blk_deq = mbuf->blk_enq;
+    }
 
-    if (can_log(mbuf)) {
+    if (can_log(mbuf))
+    {
         printf("add new blk: cnt=%lu,id=%d,size=%lu,total_alloc=%lu,data_size=%lu\n", mbuf->blk_count, blk->blk_id, size, mbuf->alloc_size, mbuf->data_size);
     }
 
@@ -76,17 +86,18 @@ void mbuf_init(mbuf_t *mbuf, uint32_t blk_size)
 void mbuf_free(mbuf_t *mbuf)
 {
     mbuf_blk_t *tmp, *blk;
-    for (blk = mbuf->blk_deq; blk && (tmp = blk->next, 1); blk = tmp) {
+    for (blk = mbuf->blk_deq; blk && (tmp = blk->next, 1); blk = tmp)
+    {
         mbuf->blk_count--;
         free(blk->buf);
         free(blk);
     }
 }
 
-
 uint32_t mbuf_add(mbuf_t *mbuf, const char *data, uint32_t size)
 {
-    if (data == NULL) return 0;
+    if (data == NULL)
+        return 0;
 
     void *p = mbuf_alloc(mbuf, size);
     memcpy(p, data, size);
@@ -99,7 +110,8 @@ uint32_t mbuf_add_span(mbuf_t *mbuf, const void *data, uint32_t size)
     mbuf_blk_t *blk = mbuf->blk_enq;
     int cap = MBUF_BLK_CAP(blk);
     char *cdat = (char *)data;
-    if (cap < size) {
+    if (cap < size)
+    {
         memcpy(blk->tail, cdat, cap);
         MBUF_ADVANCE(mbuf, blk, cap);
         size -= cap;
@@ -115,14 +127,16 @@ uint32_t mbuf_add_span(mbuf_t *mbuf, const void *data, uint32_t size)
 
 uint32_t mbuf_copy(mbuf_t *mbuf, void *buf, uint32_t size)
 {
-    if (buf == NULL) return 0;
+    if (buf == NULL)
+        return 0;
 
     mbuf_blk_t *blk = mbuf->blk_deq;
     uint32_t offset = 0;
-    while (blk && size > 0) {
+    while (blk && size > 0)
+    {
         uint32_t payload = blk->tail - blk->head;
         uint32_t data_len = payload < size ? payload : size;
-        memcpy(buf + offset, blk->head, data_len);
+        memcpy((char *)buf + offset, blk->head, data_len);
         offset += data_len;
         size -= data_len;
         blk = blk->next;
@@ -134,20 +148,23 @@ uint32_t mbuf_copy(mbuf_t *mbuf, void *buf, uint32_t size)
 void mbuf_drain(mbuf_t *mbuf, uint32_t size)
 {
     mbuf_blk_t *blk = mbuf->blk_deq;
-    while (blk && size > 0) {
+    while (blk && size > 0)
+    {
         uint32_t payload = blk->tail - blk->head;
         uint32_t data_len = payload < size ? payload : size;
         blk->head += data_len;
         mbuf->data_size -= data_len;
         size -= data_len;
 
-        if (MBUF_BLK_DATA_LEN(blk) == 0) {
+        if (MBUF_BLK_DATA_LEN(blk) == 0)
+        {
             //free blk
             mbuf_blk_t *tmp = blk;
             blk = mbuf->blk_deq = blk->next;
             mbuf->blk_count--;
             mbuf->alloc_size -= tmp->blk_size;
-            if (mbuf->blk_enq == tmp) {
+            if (mbuf->blk_enq == tmp)
+            {
                 mbuf->blk_enq = NULL;
             }
 
@@ -159,10 +176,12 @@ void mbuf_drain(mbuf_t *mbuf, uint32_t size)
 
 uint32_t mbuf_remove(mbuf_t *mbuf, void *buf, uint32_t size)
 {
-    if (buf == NULL) return 0;
+    if (buf == NULL)
+        return 0;
 
     uint32_t ret = mbuf_copy(mbuf, buf, size);
-    if (ret > 0) {
+    if (ret > 0)
+    {
         mbuf_drain(mbuf, ret);
     }
 
@@ -171,9 +190,11 @@ uint32_t mbuf_remove(mbuf_t *mbuf, void *buf, uint32_t size)
 
 const void *mbuf_pullup(mbuf_t *mbuf)
 {
-    if (mbuf->blk_count <= 0) return NULL;
+    if (mbuf->blk_count <= 0)
+        return NULL;
 
-    if (mbuf->blk_count == 1) {
+    if (mbuf->blk_count == 1)
+    {
         return mbuf->blk_deq->head;
     }
 
@@ -186,12 +207,13 @@ const void *mbuf_pullup(mbuf_t *mbuf)
     mbuf_blk_init(blk);
     blk->next = NULL;
 
-
     uint32_t offset = 0;
     mbuf_blk_t *tblk, *tmp;
-    for (tblk = mbuf->blk_deq; tblk && (tmp = tblk->next, 1); tblk = tmp) {
+    for (tblk = mbuf->blk_deq; tblk && (tmp = tblk->next, 1); tblk = tmp)
+    {
         uint32_t len = MBUF_BLK_DATA_LEN(tblk);
-        if (len > 0) {
+        if (len > 0)
+        {
             memcpy(blk->buf + offset, blk->head, len);
             offset += len;
         }
--- mbuf.h
+++ mbuf.h
@@ -40,67 +40,79 @@ struct mbuf_s
     int enable_log;
 };
 
-
-#define MBUF_ADVANCE(mbuf, blk, size) do { \
-    (mbuf)->data_size += (size); \
-    (blk)->tail += (size); \
-} while (0)
+#define MBUF_ADVANCE(mbuf, blk, size) \
+    do                                \
+    {                                 \
+        (mbuf)->data_size += (size);  \
+        (blk)->tail += (size);        \
+    } while (0)
 
 #define MBUF_BLK_CAP(blk) ((blk)->end - (blk)->tail)
 #define MBUF_BLK_DATA_LEN(blk) ((blk)->tail - (blk)->head)
 
-#define MBUF_ENQ(mbuf, data, size) do { \
-    void *p = mbuf_alloc(mbuf, size); \
-    memcpy(p, data, size); \
-} while(0)
+#define MBUF_ENQ(mbuf, data, size)        \
+    do                                    \
+    {                                     \
+        void *p = mbuf_alloc(mbuf, size); \
+        memcpy(p, data, size);            \
+    } while (0)
 
-#define MBUF_ENQ_WITH_TYPE(mbuf, data, type) do { \
-    type *p = (type *)mbuf_alloc(mbuf, sizeof(type)); \
-    *p = *data; \
-} while(0)
+#define MBUF_ENQ_WITH_TYPE(mbuf, data, type)              \
+    do                                                    \
+    {                                                     \
+        type *p = (type *)mbuf_alloc(mbuf, sizeof(type)); \
+        *p = *data;                                       \
+    } while (0)
 
 #if defined(__cplusplus)
-extern "C" {
+extern "C"
+{
 #endif
 
-//private
-mbuf_blk_t *mbuf_add_blk(mbuf_t *mbuf, uint32_t size);
+    //private
+    mbuf_blk_t *mbuf_add_blk(mbuf_t *mbuf, uint32_t size);
 
-void mbuf_init(mbuf_t *mbuf, uint32_t blk_size);
-void mbuf_free(mbuf_t *mbuf);
-//添加数据，如果当前blk空间不足，会新分配一个
-uint32_t mbuf_add(mbuf_t *mbuf, const char *data, uint32_t size);
-//添加数据，如果当前blk空间不足，会先填满当前blk，再分配一个新的
-uint32_t mbuf_add_span(mbuf_t *mbuf_t, const void *data, uint32_t size);
-//copy数据，但是不会从mbuf删除
-uint32_t mbuf_copy(mbuf_t *mbuf, void *buf, uint32_t size);
-//读取数据，同时会从mbuf中删除
-uint32_t mbuf_remove(mbuf_t *mbuf, void *buf, uint32_t size);
-//把mbuf内的数据变成连续的空间
-const void *mbuf_pullup(mbuf_t *mbuf);
-//删除mbuf中的数据
-void mbuf_drain(mbuf_t *mbuf, uint32_t size);
+    void mbuf_init(mbuf_t *mbuf, uint32_t blk_size);
+    void mbuf_free(mbuf_t *mbuf);
+    void mbuf_reset(mbuf_t *mbuf, uint32_t data_size);
+    //添加数据，如果当前blk空间不足，会新分配一个
+    uint32_t mbuf_add(mbuf_t *mbuf, const char *data, uint32_t size);
+    //添加数据，如果当前blk空间不足，会先填满当前blk，再分配一个新的
+    uint32_t mbuf_add_span(mbuf_t *mbuf_t, const void *data, uint32_t size);
+    //copy数据，但是不会从mbuf删除
+    uint32_t mbuf_copy(mbuf_t *mbuf, void *buf, uint32_t size);
+    //读取数据，同时会从mbuf中删除
+    uint32_t mbuf_remove(mbuf_t *mbuf, void *buf, uint32_t size);
+    //把mbuf内的数据变成连续的空间
+    const void *mbuf_pullup(mbuf_t *mbuf);
+    //删除mbuf中的数据
+    void mbuf_drain(mbuf_t *mbuf, uint32_t size);
 
-inline static void *mbuf_alloc(mbuf_t *mbuf, uint32_t size)
-{
-    if (mbuf->blk_enq == NULL) {
-        mbuf->blk_enq = mbuf_add_blk(mbuf, size);
-    }
-
-    for (; (uint32_t)MBUF_BLK_CAP(mbuf->blk_enq) < size; ) {
-        if (mbuf->blk_enq->next == NULL) {
-            mbuf_add_blk(mbuf, size);
-            break;
-        } else {
-            mbuf->blk_enq = mbuf->blk_enq->next;
+    inline static void *mbuf_alloc(mbuf_t *mbuf, uint32_t size)
+    {
+        if (mbuf->blk_enq == NULL)
+        {
+            mbuf->blk_enq = mbuf_add_blk(mbuf, size);
         }
 
-        assert(MBUF_BLK_DATA_LEN(mbuf->blk_enq) == 0);
-    }
+        for (; (uint32_t)MBUF_BLK_CAP(mbuf->blk_enq) < size;)
+        {
+            if (mbuf->blk_enq->next == NULL)
+            {
+                mbuf_add_blk(mbuf, size);
+                break;
+            }
+            else
+            {
+                mbuf->blk_enq = mbuf->blk_enq->next;
+            }
 
-    MBUF_ADVANCE(mbuf, mbuf->blk_enq, size);
-    return mbuf->blk_enq->tail - size;
-}
+            assert(MBUF_BLK_DATA_LEN(mbuf->blk_enq) == 0);
+        }
+
+        MBUF_ADVANCE(mbuf, mbuf->blk_enq, size);
+        return mbuf->blk_enq->tail - size;
+    }
 
 #if defined(__cplusplus)
 }
